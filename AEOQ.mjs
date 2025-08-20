2f7bf3a0450bb3ac702ab4ad6b5b0f29764abb07
AEOQ <200552650+AEOQ@users.noreply.github.com>
2025-08-20 11:52:36 +0800

    Update AEOQ.mjs

--- AEOQ.mjs
+++ AEOQ.mjs
@@ -15,7 +15,7 @@ const E = function (el, ...props) {
     if (el instanceof HTMLElement)
         return new.target ? (this.el = el) && this : new E(el);
     el = E.SVG.includes(el) ? document.createElementNS('http://www.w3.org/2000/svg', el) : document.createElement(el);
-    props = props.map(prop => prop instanceof HTMLElement ? [prop] : prop);
+    //props = props.map(prop => prop instanceof HTMLElement ? [prop] : prop);
     return E(el).set(...props);
 }
 Object.assign(E.prototype, {
