4de2aae6ee54a486abb8e0a7f278d4147f4b5019
bbx <bbx@bbx.com>
2026-08-27 18:52:40 +0800

    DG

--- diamond-grid.mjs
+++ diamond-grid.mjs
@@ -1,59 +1,45 @@
 import {E,Q} from '../AEOQ.mjs';
-const tagName = 'diamond-grid';
-Q('head').append(E('style', {id: tagName}, `
-    ${tagName} .DG-textShaping {
-        width:50%; height:100%;
-
-        &:nth-child(1) {
-            float:left;    
-            shape-outside:polygon(0% 0%,100% 0%,0% 50%,100% 100%,0% 100%);
-        }
-        &:nth-child(2) {
-            float:right; 
-            shape-outside:polygon(100% 0%,0% 0%,100% 50%,0% 100%,100% 100%);
-        }
-    }`
-));
-customElements.define(tagName, class extends HTMLElement {
+class DG extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({mode: 'open'}).append(
-            E('link', {rel: 'stylesheet',href: 'https://aeoq.github.io/diamond-grid.css', me: true}),
-            E('slot')
+            E('link', {rel: 'stylesheet',href: '../diamond-grid.css', me: true}),
+            E('slot', {onslotchange: () => this.arrange(null)}) //for removal
         );
     }
-    #oldWidth;
     connectedCallback() {
+        let sheets = this.getRootNode().adoptedStyleSheets;
+        sheets.includes(DG.#css) || sheets.push(DG.#css);
         let E_this = E(this);
-        isNaN(E_this.get('--side')) && E_this.set({'--side': '20em'});
-        isNaN(E_this.get('--gap')) && E_this.set({'--gap': '.5em'});
-        this.#shapeText();
+        let [side, gap] = E_this.get(['--side', '--gap']).map(p => parseFloat(p));
+        E_this.set(isNaN(side) ? {'--side': '20em'} : {}, isNaN(gap) ? {'--gap': '.5em'} : {});
         this.hidden = false;
-        new ResizeObserver(([entry]) => {
-            let newWidth = entry.borderBoxSize[0].inlineSize;
-            if (!newWidth || Math.abs(newWidth - this.#oldWidth) < 1) return;
-            this.#arrange();
-            this.#oldWidth = newWidth;
-        }).observe(this);
-        new MutationObserver(() => this.#arrange())
-        .observe(this, {attributeFilter: ['hidden', 'style'], childList: true, subtree: true});
+        DG.ReOb.observe(this);
     }
-    #shapeText = () => [...this.children].forEach(el => 
-        el.matches('img,:has(.DG-textShaping)') || el.prepend(E('span.DG-textShaping'), E('span.DG-textShaping'))
-    );
-    #arrange () {
+    static ReOb = new ResizeObserver(entries => {
+        let [grid, child] = [true, false].map(b => entries.find(en => en.target instanceof DG === b));
+        let width = child ? null : grid.borderBoxSize[0].inlineSize;
+        (entries.length === 1 || grid) && entries[0].target.closest(DG.tagName)?.arrange(width);
+    })
+    #W; #w; #g;
+    arrange (W, g = E(this).get('gap')) {
         let items = [...this.children];
-        items.forEach(item => item.classList.remove('DG-left','DG-right','DG-center','DG-next'));
-        items = items.filter(item => !item.hidden);
+        let w = items[0].offsetWidth;
+        if (!W && !this.#W || W === this.#W && w === this.#w && g === this.#g) return;
+        items.forEach(node => {
+            node.classList.remove('DG-left', 'DG-right', 'DG-center', 'DG-next');
+            node.matches('img,:has(.DG-textShaping)') || node.prepend(E('span.DG-textShaping'), E('span.DG-textShaping'));
+            DG.ReOb.observe(node);
+        });
+        items = items.filter(node => !node.hidden && node.style.display != 'none');
         if (!items.length) return;
 
-        let W = this.getBoundingClientRect().width,
-            g = E(this).get('gap'),
-            w = items[0].getBoundingClientRect().width;
+        this.#w = w, this.#g = g;
+        W ? this.#W = W : W = this.#W;
         let more = Math.floor((W + g) / (w + g)),
             less = Math.floor((2 * W - w + g) / 2 / (w + g));
         if (more === less)
-            return items.forEach((item, i) => item.classList.add(Math.ceil((i + 1) / more) % 2 === 0 ? 'DG-right' : 'DG-left'));
+            return items.forEach((node, i) => node.classList.add(Math.ceil((i + 1) / more) % 2 === 0 ? 'DG-right' : 'DG-left'));
 
         let n = 1, i;
         while (items[i = (more + less) * n - less]) {
@@ -65,6 +51,16 @@ customElements.define(tagName, class extends HTMLElement {
             n++;
         }
     }
-});
+    static tagName = 'diamond-grid'
+    static #css = new CSSStyleSheet()
+    static {
+        this.#css.replaceSync(`
+            span.DG-textShaping {width: 50%; height: 100%;}
+            span:nth-child(1 of .DG-textShaping) {float: left; shape-outside: polygon(0 0,100% 0,0 50%,100% 100%,0 100%);}
+            span:nth-child(2 of .DG-textShaping) {float: right; shape-outside: polygon(100% 0,0 0,100% 50%,0 100%,100% 100%);}
+        `)
+    }
+}
+customElements.define(DG.tagName, DG);
 //nw+(n-1)g=W, n=(W+g)/(w+g)
 //nw+(n-1)g=W-(w+g/2), n=(2W-w+g)/2(w+g)  
